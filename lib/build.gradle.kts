8b95f3d82759bbc70a3d388b858c22c40fa34ced
zj565061763 <565061763@qq.com>
2023-08-30 16:04:32 +0800

    kts

---
+++ lib/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    `maven-publish`
+}
+
+val libGroupId = "com.sd.lib.android"
+val libArtifactId = "aligner"
+val libVersionName = "1.0.0"
+
+android {
+    namespace = "com.sd.lib.aligner"
+    compileSdk = libs.versions.androidCompileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+    }
+
+    kotlinOptions {
+        freeCompilerArgs += "-module-name=$libGroupId.$libArtifactId"
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+dependencies {
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = libGroupId
+                artifactId = libArtifactId
+                version = libVersionName
+            }
+        }
+    }
+}
